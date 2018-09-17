ac5cf46e330e04270b691c2a62e83770a49591e3
P3 <ortizadrian628@gmail.com>
2018-09-17 11:12:18 -0700

    adrian ortiz

--- myscript.js
+++ myscript.js
@@ -1,1 +1,11 @@
+$("document").ready(function(){
+  $("#b").click(function(){
+    $(".hide").toggle();
+  })
+})
 
+$("document").ready(function(){
+  $("#a").click(function(){
+    $(".gone").toggle();
+  })
+})
